5ea57ec345b9da13502bd8b6f5ec24d4e27ac12c
gabicep <gabrielle.ronchi.batista@escola.pr.gov.br>
2025-06-25 17:01:06 -0300

    Add files via upload
    aula 06

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const BotaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-BotaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave (){
     const texto = document.querySelector('#entrada-de-texto').value;
@@ -14,17 +14,29 @@ function mostraPalavrasChave (){
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
 
-    let frequencias = {};
-    for (let i of palavras){
-        frequencias[i] = 0;
-        for(let j of palavras){
-            if (i == j){
-                frequencias[i]++;
-            }
-        }
+    const frequencias = contaFrequencias(palavras);
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+   return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+  let frequencias = {};
+
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
     }
+  }
 
-    console.log(frequencias);
-
-    return palavras;
+  return frequencias;
 }
