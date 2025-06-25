a0eedd54ffd8bc1d6b577f11c636b1c9722c0ad3
gabicep <gabrielle.ronchi.batista@escola.pr.gov.br>
2025-06-25 15:43:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const BotaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+BotaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave (){
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave')
+    const palavras = texto.split (" ");
+    
+    campoResultado.textContent = palavras.join (", ");
+}
